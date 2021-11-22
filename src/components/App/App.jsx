4ae5b2713122ca2b41c9049aa93d56bc65300b95
POBSIZ <65818008+POBSIZ@users.noreply.github.com>
2021-11-22 12:29:11 +0900

    modified webpack publick path

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, Route, BrowserRouter } from "react-router-dom";
 import { hot } from 'react-hot-loader';
 import PropTypes from 'prop-types';
@@ -6,14 +6,12 @@ import { Helmet } from 'react-helmet';
 
 import './assets/css/App.scss';
 
-class App extends React.Component {
-    render() {
-        return (
-            <div className="main">
-                <h1>Webpack with React</h1>
-            </div>             
-        );
-    }
-}
+const App = () => {
+    return (
+        <>
+            <h1>Webpack with React</h1>
+        </>
+    );
+};
 
-export default hot(module)(App);
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Link, Route, BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from "react-redux";
+import { CookiesProvider } from 'react-cookie';
+import axios from 'axios';
+import { createHashHistory, createHistory } from 'history';
+
+const history = createHistory();
 
 // Components
 import App from './components/App/App';
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
-    <App/>,
-    rootElement
+    <App></App>
+    ,rootElement
 );
